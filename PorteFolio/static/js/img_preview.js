2314341de40853d07e25679424a39c176fadc6d4
Greg M <marainigregory@pm.me>
2023-07-12 11:13:01 +0200

    image preview and sidebar added

--- PorteFolio/static/js/img_preview.js
+++ PorteFolio/static/js/img_preview.js
@@ -1,59 +1,19 @@
-function previewImage(input) {
-  var preview = document.getElementById("image-preview");
-  var file = input.files[0];
-  var reader = new FileReader();
+document.getElementById("edit-img").addEventListener("change", function(event) {
+  var output = document.getElementById("image-output");
+  output.innerHTML = ""; // Réinitialise l'aperçu de l'image
 
-  reader.onloadend = function() {
-    preview.src = reader.result;
-
-    var desiredWidth = 800; // Largeur souhaitée en pixels
-    var scaleFactor = desiredWidth / preview.naturalWidth; // Calcul du facteur d'échelle
-
-    // Réinitialise la transformation d'échelle précédente uniquement si le fichier a été modifié
-    if (input.value) {
-      preview.style.transform = '';
+  var files = event.target.files;
+  if (files && files.length > 0) {
+    var file = files[0];
+    if (file.type.match(/^image\//)) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var img = document.createElement("img");
+        img.src = e.target.result;
+        img.classList.add("preview-image");
+        output.appendChild(img);
+      };
+      reader.readAsDataURL(file);
     }
-
-    // Applique la nouvelle transformation d'échelle
-    preview.style.transform = `scale(${scaleFactor})`;
-
-    var dataForm = document.querySelector('.data-form');
-    var formElements = dataForm.querySelectorAll('input, textarea, select');
-    var maxWidth = 0;
-    formElements.forEach(function(element) {
-      maxWidth = Math.max(maxWidth, element.scrollWidth);
-    });
-    dataForm.style.width = maxWidth + 'px';
-
-    var parentWidth = preview.parentNode.clientWidth; // Obtenir la largeur du parent
-    preview.style.maxWidth = parentWidth + "px"; // Définir la largeur maximale de l'élément preview
-  };
-
-  if (file) {
-    reader.readAsDataURL(file);
-  } else {
-    preview.src = "";
-    preview.style.transform = ""; // Réinitialise la transformation de l'image si aucun fichier n'est sélectionné
-
-    var dataForm = document.querySelector('.data-form');
-    dataForm.style.width = ""; // Réinitialise la largeur du conteneur si aucun fichier n'est sélectionné
   }
-}
-
-window.addEventListener('load', function() {
-  var dataForm = document.querySelector('.data-form');
-  var formElements = dataForm.querySelectorAll('input, textarea, select');
-
-  function adjustDataFormSize() {
-    var maxWidth = 0;
-    formElements.forEach(function(element) {
-      maxWidth = Math.max(maxWidth, element.scrollWidth);
-    });
-
-    dataForm.style.width = maxWidth + 'px';
-  }
-
-  adjustDataFormSize();
-
-  window.addEventListener('resize', adjustDataFormSize);
 });
--- PorteFolio/static/js/sidebar.js
+++ PorteFolio/static/js/sidebar.js
@@ -1,29 +1,59 @@
 $(document).ready(function() {
-    // Sélectionnez la flèche et la sidebar
-    var arrow = $('#arrow');
-    var sidebar = $('#sidebar');
+  // Sélectionnez les flèches et les sidebars
+  var arrowLeft = $('#arrow_left');
+  var sidebarLeft = $('#sidebar_left');
+  var arrowRight = $('#arrow_right');
+  var sidebarRight = $('#sidebar_right');
 
-    // Événement de survol de la flèche
-    arrow.hover(
-        function() {
-            sidebar.addClass('active'); // Ajoute la classe .active à la sidebar
-            arrow.addClass('active'); // Ajoute la classe .active à la flèche
-        },
-        function() {
-            sidebar.removeClass('active'); // Supprime la classe .active de la sidebar
-            arrow.removeClass('active'); // Supprime la classe .active de la flèche
-        }
-    );
+  // Fonction pour ajouter la classe .active aux sidebars et aux flèches
+  function activateSidebar(sidebar, arrow) {
+    sidebar.addClass('active'); // Ajoute la classe .active à la sidebar
+    arrow.addClass('active'); // Ajoute la classe .active à la flèche
+  }
 
-    // Événement de survol de la sidebar
-    sidebar.hover(
-        function() {
-            sidebar.addClass('active'); // Ajoute la classe .active à la sidebar
-            arrow.addClass('active'); // Ajoute la classe .active à la flèche
-        },
-        function() {
-            sidebar.removeClass('active'); // Supprime la classe .active de la sidebar
-            arrow.removeClass('active'); // Supprime la classe .active de la flèche
-        }
-    );
+  // Fonction pour supprimer la classe .active des sidebars et des flèches
+  function deactivateSidebar(sidebar, arrow) {
+    sidebar.removeClass('active'); // Supprime la classe .active de la sidebar
+    arrow.removeClass('active'); // Supprime la classe .active de la flèche
+  }
+
+  // Gestionnaire d'événements pour la sidebar gauche
+  sidebarLeft.hover(
+    function() {
+      activateSidebar(sidebarLeft, arrowLeft);
+    },
+    function() {
+      deactivateSidebar(sidebarLeft, arrowLeft);
+    }
+  );
+
+  // Gestionnaire d'événements pour la flèche gauche
+  arrowLeft.hover(
+    function() {
+      activateSidebar(sidebarLeft, arrowLeft);
+    },
+    function() {
+      deactivateSidebar(sidebarLeft, arrowLeft);
+    }
+  );
+
+  // Gestionnaire d'événements pour la sidebar droite
+  sidebarRight.hover(
+    function() {
+      activateSidebar(sidebarRight, arrowRight);
+    },
+    function() {
+      deactivateSidebar(sidebarRight, arrowRight);
+    }
+  );
+
+  // Gestionnaire d'événements pour la flèche droite
+  arrowRight.hover(
+    function() {
+      activateSidebar(sidebarRight, arrowRight);
+    },
+    function() {
+      deactivateSidebar(sidebarRight, arrowRight);
+    }
+  );
 });
