d45e27be497958811c1e46add29515004baa9069
greg <marianigregory@pm.me>
2023-08-24 13:28:19 +0200

    properties test

--- PorteFolio/static/js/selection_logic.js
+++ PorteFolio/static/js/selection_logic.js
@@ -3,6 +3,75 @@ import * as THREE from 'three';
 let previousSelectedObject = null;
 let originalMaterial = null;
 
+const attributesToShow = [
+  'ObjectType.value',
+  'Name.value',
+  'Description.value',
+  'GlobalId.value',
+  'expressID',
+  // Ajoutez d'autres chemins d'attributs que vous souhaitez afficher ici
+];
+
+function findLevelAndBuilding(props) {
+  let level = null;
+  let building = null;
+
+  function traverse(obj) {
+    if (obj.hasOwnProperty('Level')) {
+      level = obj['Level'].value; // ou simplement obj['Level'] selon la structure
+    }
+    if (obj.hasOwnProperty('Building')) {
+      building = obj['Building'].value; // ou simplement obj['Building']
+    }
+    for (const key in obj) {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        traverse(obj[key]); // Parcours récursif des propriétés imbriquées
+      }
+    }
+  }
+
+  traverse(props);
+  return { level, building };
+}
+
+function listAttributes(obj, prefix = '') {
+  let attributes = [];
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      // Si la propriété est un objet, explorez récursivement ses attributs
+      attributes = attributes.concat(listAttributes(obj[key], prefix + key + '.'));
+    } else {
+      // Sinon, ajoutez l'attribut à la liste
+      attributes.push(prefix + key);
+    }
+  }
+  return attributes;
+}
+
+function getNestedValue(obj, path) {
+  const keys = path.split('.');
+  let value = obj;
+  for (const key of keys) {
+    if (value && key in value) {
+      value = value[key];
+    } else {
+      return null;
+    }
+  }
+  return value;
+}
+
+function formatProperties(props) {
+  let result = '';
+  for (const path of attributesToShow) {
+    const value = getNestedValue(props, path);
+    if (value !== null) {
+      result += `${path}: ${value}\n`;
+    }
+  }
+  return result;
+}
+
 export async function handleObjectSelection(event, viewer, textField) {
     const pickedItem = await viewer.IFC.selector.pickIfcItem(true);
 
@@ -16,6 +85,8 @@ export async function handleObjectSelection(event, viewer, textField) {
 
         const props = await viewer.IFC.getProperties(pickedItem.modelID, pickedItem.id, true, false);
         console.log('Propriétés de l\'élément IFC:', props);
+        const attributes = listAttributes(props);
+        console.log("Attributs disponibles:", attributes);
 
         originalMaterial = pickedItem.material;  // Stockez le matériau original
         const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
@@ -24,7 +95,8 @@ export async function handleObjectSelection(event, viewer, textField) {
         // Afficher les informations de l'objet dans un champ textuel
 //        material + properties
 //        textField.value = JSON.stringify(pickedItem, null, 2) + "\n\nProperties:\n" + JSON.stringify(props, null, 2);
-        textField.value = "Properties:\n" + JSON.stringify(props, null, 2);
+//        textField.value = "Properties:\n" + JSON.stringify(props, null, 2);
+  textField.value = "Properties:\n" + formatProperties(props);
 
         previousSelectedObject = pickedItem;
     } else {
