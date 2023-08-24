a4845d57325b51001a2a1873e400af65a668406d
greg <marianigregory@pm.me>
2023-08-24 13:28:44 +0200

    CSV testing + dependances updates

--- PorteFolio/IFCextract/ifc_class.py
+++ PorteFolio/IFCextract/ifc_class.py
@@ -2,7 +2,7 @@ import os
 import zipfile
 
 from contextlib import contextmanager
-
+from .src.ifccsv import IfcCsv
 import ifcopenshell
 import ifcopenshell.geom
 import ifcopenshell.util.element as Element
@@ -75,6 +75,26 @@ class IFCObjectAnalyzer:
         list_of_element_types = list(set(element.is_a() for element in self.filtered_entities))
         return list_of_element_types
 
+    def get_spatial_info(self, element):
+        level_name = ""
+        building_name = ""
+        site_name = ""
+
+        # Parcourir la structure spatiale pour obtenir les informations hiérarchiques
+        spatial_element = element
+        while spatial_element:
+            print(spatial_element)
+            if spatial_element.is_a("IfcBuildingStorey"):
+                level_name = spatial_element.Name or ""
+            elif spatial_element.is_a("IfcBuilding"):
+                building_name = spatial_element.Name or ""
+            elif spatial_element.is_a("IfcSite"):
+                site_name = spatial_element.Name or ""
+
+            spatial_element = Element.get_container(spatial_element)
+
+        return level_name, building_name, site_name
+
     def extract_data(self, element_type):
         def add_pset_attributes(psets):
             for pset_name, pset_data in psets.items():
@@ -92,15 +112,18 @@ class IFCObjectAnalyzer:
             add_pset_attributes(psets)
             qtos = Element.get_psets(element, qtos_only=True)
             add_pset_attributes(qtos)
+
+            level_name, building_name, site_name = self.get_spatial_info(element)
+
             datas.append({
                 "ExpressID": element.id(),
                 "GlobalID": element.GlobalId,
                 "Class": element.is_a(),
                 "PredefinedType": Element.get_predefined_type(element),
                 "Name": container_name,
-                "Level": Element.get_container(element).Name
-                if Element.get_container(element)
-                else "",
+                "Level": level_name,
+                "Building": building_name,
+                "Site": site_name,
                 "ObjectType": Element.get_type(element).Name
                 if Element.get_type(element)
                 else "",
@@ -112,25 +135,52 @@ class IFCObjectAnalyzer:
     def export_ifc_to_csv(self):
         all_types = self.get_all_element_types()
         csv_files = []
-        for type in all_types:
-            data, pset_attributes = self.extract_data(type)
-            attributes = ["ExpressID", "GlobalID", "Class", "PredefinedType", "Name", "Level",
-                          "ObjectType"] + pset_attributes
-            pandas_data = []
-            for obj_data in data:
-                row = []
-                for attribute in attributes:
-                    value = get_attribute_value(obj_data, attribute)
-                    row.append(value)
-                pandas_data.append(row)
-            dataframe = pd.DataFrame.from_records(pandas_data, columns=attributes)
-            csv_path = f'media/csv/{type}.csv'
-            dataframe.to_csv(csv_path)
-            csv_files.append(csv_path)
         zip_path = 'media/zip/output.zip'
+
+        for element_type in all_types:
+            # Sélectionner les éléments de ce type
+            elements = ifcopenshell.util.selector.Selector.parse(self.ifc_model, f".{element_type}")
+
+            # Spécifier les attributs à exporter (adaptez cela selon vos besoins)
+            attributes = ["ExpressID", "GlobalID", "Class", "PredefinedType", "Name", "Level", "ObjectType"]
+
+            # Créer un objet IfcCsv et exporter vers CSV
+            ifc_csv = IfcCsv()
+            csv_path = f'media/csv/{element_type}.csv'
+            ifc_csv.export(self.ifc_model, elements, attributes, output=csv_path, format="csv", delimiter=",", null="-")
+
+            # Ajouter le chemin du fichier CSV à la liste pour la compression
+            csv_files.append(csv_path)
+
+        # Compresser les fichiers CSV dans un fichier ZIP
         for csv_file in csv_files:
             compress_to_zip_file(csv_file, zip_path)
             with delete_file_after_use(csv_file):
                 pass  # Le fichier CSV sera supprimé après ce contexte
+
         return zip_path
 
+    # def export_ifc_to_csv(self):
+    #     all_types = self.get_all_element_types()
+    #     csv_files = []
+    #     for type in all_types:
+    #         data, pset_attributes = self.extract_data(type)
+    #         attributes = ["ExpressID", "GlobalID", "Class", "PredefinedType", "Name", "Level",
+    #                       "ObjectType"] + pset_attributes
+    #         pandas_data = []
+    #         for obj_data in data:
+    #             row = []
+    #             for attribute in attributes:
+    #                 value = get_attribute_value(obj_data, attribute)
+    #                 row.append(value)
+    #             pandas_data.append(row)
+    #         dataframe = pd.DataFrame.from_records(pandas_data, columns=attributes)
+    #         csv_path = f'media/csv/{type}.csv'
+    #         dataframe.to_csv(csv_path)
+    #         csv_files.append(csv_path)
+    #     zip_path = 'media/zip/output.zip'
+    #     for csv_file in csv_files:
+    #         compress_to_zip_file(csv_file, zip_path)
+    #         with delete_file_after_use(csv_file):
+    #             pass  # Le fichier CSV sera supprimé après ce contexte
+    #     return zip_path
